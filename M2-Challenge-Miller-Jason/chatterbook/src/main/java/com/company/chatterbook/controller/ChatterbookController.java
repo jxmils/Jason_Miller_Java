b0c0011ff333232306581f88fb8bda4809ee6856
Jason Paul Miller <jamiller@cs.nmsu.edu>
2023-06-26 23:40:12 -0700

    completed M2 challenge, uploading working files

---
+++ M2-Challenge-Miller-Jason/chatterbook/src/main/java/com/company/chatterbook/controller/ChatterbookController.java
@@ -0,0 +1,66 @@
+package com.company.chatterbook.controller;
+import java.util.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.company.chatterbook.models.User;
+import com.company.chatterbook.models.ChatterPost;
+
+@RestController
+public class ChatterbookController {
+    private List<User> userList;
+
+    public ChatterbookController() {
+        User luis = new User("Luis");
+        User sue = new User("Sue");
+        User timothy = new User("Timothy");
+        User george = new User("George");
+        User arturo = new User("Arturo");
+        User mariella = new User("Mariella");
+        User paolo = new User("Paolo");
+        User tri = new User("Tri");
+        User jane = new User("Jane");
+        User carol = new User("Carol");
+        User carl = new User("Carl");
+
+        luis.setChatterPosts(Arrays.asList(new ChatterPost("Hey! This is my first post!"), new ChatterPost("Anybody want to be friends?")));
+        sue.setChatterPosts(Arrays.asList(new ChatterPost("I'm bored"), new ChatterPost("Who wants to hang?")));
+        timothy.setChatterPosts(Arrays.asList(new ChatterPost("My life is awesome!"), new ChatterPost("Click here to buy my vegan shakes!")));
+        george.setChatterPosts(Arrays.asList(new ChatterPost("I like pigs."), new ChatterPost("I love lamp.")));
+        arturo.setChatterPosts(Arrays.asList(new ChatterPost("My cat is so cute"), new ChatterPost("My kitten is purr-fect.")));
+        mariella.setChatterPosts(Arrays.asList(new ChatterPost("I'll never post again")));
+        paolo.setChatterPosts(Arrays.asList(new ChatterPost("Have you ever heard of the band Nirvana?"), new ChatterPost("Pearl Jam 4 Life")));
+        tri.setChatterPosts(Arrays.asList(new ChatterPost("You definitely grew up in the 90s if you get these 10 references."), new ChatterPost("I don't get this generation? Everyone expects a participation trophy.")));
+        jane.setChatterPosts(Arrays.asList(new ChatterPost("I just wrecked my dad's car. He's going to kill me!"), new ChatterPost("Grounded.... for 5 months :( ")));
+        carol.setChatterPosts(Arrays.asList(new ChatterPost("Does anyone have some imodium?")));
+        carl.setChatterPosts(Arrays.asList(new ChatterPost("My roommate is awful!!!!"), new ChatterPost("Anyone know a room for rent in Hyde Park?")));
+
+        userList = Arrays.asList(luis, sue, timothy, george, arturo, mariella, paolo, tri, jane, carol, carl);
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public List<User> getAllUsers() {
+        return userList;
+    }
+    @RequestMapping(value = "/users/{username}", method = RequestMethod.GET)
+    public User getUserByUsername(@PathVariable String username) {
+        for (User user : userList) {
+            if (user.getName().equals(username)) {
+                return user;
+            }
+        }
+        return null; // User not found
+    }
+
+    @RequestMapping(value = "/users/{username}/chatterPosts", method = RequestMethod.GET)
+    public List<ChatterPost> getChatterPostsByUsername(@PathVariable String username) {
+        for (User user : userList) {
+            if (user.getName().equals(username)) {
+                return user.getChatterPosts();
+            }
+        }
+        return null; // User not found
+    }
+}
---
+++ M2-Challenge-Miller-Jason/chatterbook/src/main/java/com/company/chatterbook/models/ChatterPost.java
@@ -0,0 +1,19 @@
+package com.company.chatterbook.models;
+
+public class ChatterPost {
+    private String text;
+
+    public ChatterPost() {}
+
+    public ChatterPost(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
